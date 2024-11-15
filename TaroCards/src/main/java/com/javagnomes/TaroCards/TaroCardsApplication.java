d8975c7b3799f78496d71b794bd9f12df13b4987
Vainslav <Vladik_ww@mail.ru>
2024-11-15 22:18:50 +0300

    Initialized taro cards

---
+++ TaroCards/src/main/java/com/javagnomes/TaroCards/TaroCardsApplication.java
@@ -0,0 +1,13 @@
+package com.javagnomes.TaroCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaroCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaroCardsApplication.class, args);
+	}
+
+}
